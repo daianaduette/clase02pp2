4775376f8fde6ead3ae5e097b7b016a65e9e09ae
daianaduette <daiana.duette@hotmail.com>
2021-04-18 19:10:27 -0300

    ejercicio3

---
+++ app/ejercicio3.php
@@ -0,0 +1,42 @@
+
+//al ingresar un mes del año informar si es invierno, verano, otoño o primavera. 
+
+<?php
+
+$mes= rand (1 , 12);
+
+switch($mes)
+{
+    case 1 = "Verano";
+    break;
+    case 2 :"Verano";
+    break;
+    case 3 : "Otoño";
+    break;
+    case 4 : "otoño";
+    break;
+    case 5 : "otoño";
+    break;
+    case 6 : "invierno";
+    break:
+    case 7 : "invierno";
+    break;
+    case 8 : "invierno";
+    break;
+    case 9 : "primavera";
+    break;
+    case 10 : "primavera";
+    break;
+    case 11 : "primavera";
+    break;
+    case 12 : "verano";
+    break;
+
+}
+if (mes >=1 && mes <=12){
+
+   echo ("el mes pertenece a la estacion ".$mes .$estacion);
+
+}
+
+?>
