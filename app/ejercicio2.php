0f6100e79583b4d1f82b8b0211f4517c13a5a96a
daianaduette <daiana.duette@hotmail.com>
2021-04-18 16:13:03 -0300

    ejercicio2

--- app/ejercicio2.php
+++ app/ejercicio2.php
@@ -4,9 +4,9 @@ $precio = rand (1-100);
 
 $precioMasIva = $precio * 1.21;
 
-echo "Precio sin Iva" $precio;
+echo "Precio sin Iva" . $precio;
 
-echo "Precio con Iva" $precioMasIva";
+echo "Precio con Iva" . $precioMasIva";
 
 
 
