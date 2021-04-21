6417405e52b8c6fb61207832d15b205f9ca75293
daianaduette <daiana.duette@hotmail.com>
2021-04-21 20:47:12 -0300

    ejercicio4

--- app/ejercicio4.php
+++ app/ejercicio4.php
@@ -5,16 +5,16 @@ $numero2 = 20;
 $numero3 = 30;
 
 if($numero1 > $numero2){
-    echo "es el numero mayor" .$numero1;
+    echo "es el numero mayor de los tres es :" .$numero1;
 
 }
 
 if ($numero2 > $numero3){
-    echo "es el numero mayor " .$numero2;
+    echo "es el numero mayor de los tres es: " .$numero2;
 
 }
 if ($numero3 > $numero2 && $numero3 > $numero1){
-    echo "es el numero mayor" .$numero3;
+    echo "es el numero mayor de los tres es: " .$numero3;
 }
 
 
