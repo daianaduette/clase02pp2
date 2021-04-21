1e927c456a4e69dffb0d07287f50cb96a07fd537
daianaduette <daiana.duette@hotmail.com>
2021-04-21 19:59:23 -0300

    ejercicio2

--- app/ejercicio2.php
+++ app/ejercicio2.php
@@ -5,9 +5,9 @@ $precio = "1.99";
 
 $precioMasIva = ($precio * 1.21);
 
-echo "Precio sin Iva" . $precio;
+echo " Precio sin Iva " . $precio;
 
-echo "Precio con Iva" . $precioMasIva;
+echo " Precio con Iva " . $precioMasIva;
 
 
 
