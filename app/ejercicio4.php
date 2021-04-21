0f0b8ab2c1acedfb42314c0323aefaea17e82932
daianaduette <daiana.duette@hotmail.com>
2021-04-21 20:40:18 -0300

    ejercicio4

---
+++ app/ejercicio4.php
@@ -0,0 +1,22 @@
+<?php
+
+$numero1 = 10;
+$numero2 = 20;
+$numero3 = 30;
+
+if($numero1>$numero2){
+    echo "es el numero mayor" .$numero1 ;
+
+}
+
+if ($numero2> $numero3){
+    echo "es el numero mayor " .$numero2;
+
+}
+if ($numero 3> $numero2 && $numero3>$numero1){
+    echo "es el numero mayor" .$numero3;
+}
+
+
+
+?>
