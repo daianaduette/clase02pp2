bf6ab8616c033787afce803238160ea2728b6fb0
daianaduette <daiana.duette@hotmail.com>
2021-04-23 20:48:17 -0300

    ejercicio5

--- app/ejercicio5.php
+++ app/ejercicio5.php
@@ -3,28 +3,36 @@
 //obtener la edad informar si es mayor adolescente o menor de edad con switch
 //niño-adolescente-mayor
 
-$edad = 15;
-
+$edad = rand (1,100);
 
 
 switch ($edad)
 {
-case "10":
+case 1:
+case 2:
+case 3:
+case 4:
+case 5:
+case 6:
+case 7:
+case 8:
+case 9:
+case 10:
+case 11:
+case 12:
+case 13:
+case 14:
     echo "Es un niño";
     break;
- case "15":
+case 15:
+case 16:
+case 17:
     echo "Es un adolescente";
     break;
-  case "18":
+ case 18:
     echo "Es mayor";
     break;
 
-
-
-
 }
-
-
-
 
 ?>
